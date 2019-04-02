381e5a6e8ccbce70b7340a80fb7e3a38c9435c08
visualstudio <visualstudio@125-PC11>
2019-04-02 17:09:46 -0400

    method

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp6
+
+// student name Simranjeet Kaur
+// student name Anmoldeep Singh
+// student Id C0726122
+// student Id C0730317
+// Csd3354 Section 2
+//Assignment 2
+// march 6,2019
+
+namespace DelegatesAndEvents
+{
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1(int i)
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+
+    }
+}
+
+namespace DelegatesAndEvents
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
         }
     }
 }
